4ebb9c1e89dcef6797d198614133bef3a63079b5
Yohan <prakoso.yohan@gmail.com>
2024-03-22 14:54:23 +0800

    update sequelize project

---
+++ project/config/config.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+const config = {
+  development: {
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DB,
+    host: process.env.PG_HOST,
+    dialect: 'postgres'
+  }
+};
+
+// console.log(config);
+
+module.exports = config
